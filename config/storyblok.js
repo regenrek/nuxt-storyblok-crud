a849a458612258f90b2f41f862cd2d32f9a242b9
Kevin Regenrek <hello@regenrek.at>
2020-11-22 20:28:13 +0100

    remove redundant spaceid

--- config/storyblok.js
+++ config/storyblok.js
@@ -1,7 +1,7 @@
 export default {
   accessToken: process.env.SB_CLIENT_ACCESS_TOKEN,
   oAuthToken: process.env.SB_MANAGEMENT_OAUTH_TOKEN,
-  spaceId: process.env.SB_CLIENT_SPACE_ID,
+  spaceId: process.env.SB_SPACE_ID,
   defaultLanguage: process.env.DEFAULT_LANGUAGE || 'en',
   cacheProvider: 'memory',
   version: 'auto',
